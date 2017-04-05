4b764b8ef0a41179bf447aeabaa9786bffbdbcf0
danihcv <daniel.vassalo@hotmail.com.br>
2017-04-05 19:35:53 -0300

    criacao do projeto

---
+++ cmake-build-debug/CMakeFiles/HUFFMAN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HUFFMAN.dir/main.c.obj"
+  "HUFFMAN.pdb"
+  "HUFFMAN.exe"
+  "HUFFMAN.exe.manifest"
+  "libHUFFMAN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HUFFMAN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
